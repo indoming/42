019e2984cd68b3d0eaaedd2a2cb4452367a1cf52
Inigo Dominguez <indoming@c3r3s2.42urduliz.com>
2021-08-06 13:52:40 +0200

    Mandatory

--- libft/library/libft.h
+++ libft/library/libft.h
@@ -6,7 +6,7 @@
 /*   By: indoming <indoming@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 12:19:45 by indoming          #+#    #+#             */
-/*   Updated: 2021/07/27 13:31:36 by indoming         ###   ########.fr       */
+/*   Updated: 2021/08/06 12:30:10 by indoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+void	*content;
+struct	s_list *next;
+} t_list;
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
@@ -38,5 +45,16 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 char	*ft_strdup(const char *s1);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+char	**ft_spit(char const *s, char c);
 
 #endif
